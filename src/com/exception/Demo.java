bb297cec58a76ae97529478271b7b0a21e1c71fb
rameshghule11 <rameshghule112002@gmail.com>
2026-06-25 11:59:05 +0530

    Exception code Add

--- src/com/exception/Demo.java
+++ src/com/exception/Demo.java
@@ -4,5 +4,15 @@ public class Demo {
 
 	 public static void main(String[] args) {
 		
+		 System.out.println("Hello My name is banti ");
+		 System.out.println(10+10);
+		 try {
+		 System.out.println(10/0);
+		 }catch(ArithmeticException e) {
+			 System.out.println("we cant Divide by 0");
+		 }
+		 System.out.println("I am From pune ");
+		 System.out.println("I am a Student ");
+		 System.out.println("I like to play Video game And BGMI");
 	}
 }
